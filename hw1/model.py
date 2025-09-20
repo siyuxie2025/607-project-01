86a54d3f313ac24994173ede92c177fccfd47283
Siyu Xie <siyuxie@l-kgxr32k9rm.local>
2025-09-20 10:34:36 -0400

    Combined run_analysis and save_result functiosn in run_analysis function

--- hw1/model.py
+++ hw1/model.py
@@ -17,6 +17,17 @@ def run_analysis(X_train, X_test, y_train, y_test):
     report = classification_report(y_test, y_pred, output_dict=True)
     cm = confusion_matrix(y_test, y_pred)
 
+    pd.DataFrame(report).to_json('results/report/model_results.json')
+    pd.to_pickle(model, 'results/report/logistic_regression_model.pkl')
+
+    plt.figure(figsize=(6, 5))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Not Fraud", "Fraud"],
+                 yticklabels=["Not Fraud", "Fraud"])
+    plt.title("Confusion Matrix")
+    plt.ylabel("True")
+    plt.xlabel("Predicted")
+    plt.savefig('results/figures/confusion_matrix.png')
+
     return model, report, cm
 
 
@@ -27,14 +38,6 @@ def show_results(report, cm):
     report (dict): Classification report as a dictionary.
     cm (array): Confusion matrix.
     """
-    
-    print("Classification Report:")
-    for label, metrics in report.items():
-        print(label, metrics)
 
-    plt.figure(figsize=(6, 5))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Not Fraud", "Fraud"], yticklabels=["Not Fraud", "Fraud"])
-    plt.title("Confusion Matrix")
-    plt.ylabel("True")
-    plt.xlabel("Predicted")
-    plt.show()
+   
+    # plt.show()
