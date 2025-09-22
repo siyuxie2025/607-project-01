7661e5208c36be01909dbdacbd182720dcfd6112
Siyu Xie <siyuxie@l-kgxr32k9rm.local>
2025-09-21 20:30:16 -0400

    Updated hw1/run_analysis.py, added printing information

--- hw1/run_analysis.py
+++ hw1/run_analysis.py
@@ -3,8 +3,10 @@ from EDA import eda_features, eda_cvv_match, eda_transaction_amount, eda_categor
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-from model import run_analysis, show_results
+from model import run_analysis
+import warnings
 
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 if __name__ == "__main__":
 
@@ -14,18 +16,25 @@ if __name__ == "__main__":
     data_find_duplicates(data)
 
     preprocessed_data = data_preprocessing(data)
+    print("Preprocessing completed.")
     
     #save preprocessed data
     preprocessed_data.to_csv('data/processed/preprocessed_data.csv', index=False)
+    print("Preprocessed data saved to data/processed/preprocessed_data.csv")
 
     # EDA part in EDA.py
     eda_features(preprocessed_data)
+    print("EDA on features completed.")
     eda_cvv_match(preprocessed_data)
+    print("EDA on CVV match completed.")
     eda_transaction_amount(preprocessed_data)
+    print("EDA on transaction amount completed.")
     eda_categorical_features(preprocessed_data)
-
+    print("EDA on categorical features completed.")
 
     X_train, X_test, y_train, y_test = train_test_split_data(preprocessed_data, 
                                                              target_column='isFraud')
+    print("Train-test split completed.")
     
     run_analysis(X_train, X_test, y_train, y_test)
+    print("Model training and evaluation completed.")
